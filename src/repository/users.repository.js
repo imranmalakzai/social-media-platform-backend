07a3a04720b04473b19631113008558c62cd43ce
imranmalakzai <afghanimran92@gmail.com>
2026-02-12 15:05:31 +0430

    feat(db): add create new user database query

---
+++ src/repository/users.repository.js
@@ -0,0 +1,10 @@
+import { pool } from "../config/db.config.js";
+
+// create new user
+export const create = async (user) => {
+  const [result] = await pool.query(
+    "INSERT INTO users (username,email,password,bio,profileImage,coverImage) VALUES (?,?,?,?,?,?)",
+    [user.username, user.email, user.bio, user.profileImage, user.coverImage],
+  );
+  return result.insertId;
+};
