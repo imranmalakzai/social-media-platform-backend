e089eec384f167fa8739246ed2242eb262f47ead
imranmalakzai <afghanimran92@gmail.com>
2026-02-12 12:56:03 +0430

    refc(env): add env varibles for export

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -1,3 +1,14 @@
 import env from "dotenv";
 env.configDotenv({ path: ".env" });
-export const {} = process.env;
+export const {
+  PORT,
+  DB_NAME,
+  USER,
+  HOST,
+  PASSWORD,
+  CORS_ORIGIN,
+  JWT_ACCESS_TOKEN_SECRET,
+  JWT_REFRESH_TOKEN_SECRET,
+  JWT_ACCESS_TOKEN_EXPIRY,
+  JWT_REFRESH_TOKEN_EXPIRY,
+} = process.env;
