fd7f789a984cf3b9ae00650653104130399c1ac7
imranmalakzai <afghanimran92@gmail.com>
2026-02-13 18:49:38 +0430

    feat(db): add delete a post database query

--- src/repository/likes.repository.js
+++ src/repository/likes.repository.js
@@ -8,3 +8,12 @@ export const create = async (user_id, post_id) => {
   );
   return result.insertId;
 };
+
+// Delete (unlike a post)
+export const remove = async (user_id, post_id) => {
+  const [result] = await pool.query(
+    "DELETE FROM likes WHERE (user_id,post_id) VALUES (?,?)",
+    [user_id, post_id],
+  );
+  return result.affectedRows;
+};
