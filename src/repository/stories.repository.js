89f817f51fbc9cbb57d86d3e22d8454f231cab1a
imranmalakzai <afghanimran92@gmail.com>
2026-02-13 06:44:51 +0430

    fix(db): correct create new story database query

--- src/repository/stories.repository.js
+++ src/repository/stories.repository.js
@@ -2,13 +2,13 @@ import { pool } from "../config/db.config.js";
 
 // create new story
 export const create = async (story) => {
-  const [rows] = await pool.query(
+  const [result] = await pool.query(
     `INSERT INTO stories (user_id, image, expires_at)
      VALUES (?, ?, DATE_ADD(NOW(), INTERVAL 24 HOUR))`,
     [story.user_id, story.image],
   );
 
-  return rows;
+  return result.insertId;
 };
 
 // Delete story
