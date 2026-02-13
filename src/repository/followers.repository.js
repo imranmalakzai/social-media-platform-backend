95a3ceab4371e7757d9c5e07843910bfd02bbf1f
imranmalakzai <afghanimran92@gmail.com>
2026-02-13 07:27:03 +0430

    feat(db): add unfollow a user database query

--- src/repository/followers.repository.js
+++ src/repository/followers.repository.js
@@ -8,3 +8,12 @@ export const create = async (follower_id, following_id) => {
   );
   return result.insertId;
 };
+
+// Delete (unfollow a user)
+export const remove = async (follower_id, following_id) => {
+  const [result] = await pool.query(
+    "DELETE FROM follows WHERE follower_id = ?, and following_id = ?",
+    [follower_id, following_id],
+  );
+  return result.affectedRows;
+};
