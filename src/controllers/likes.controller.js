8ac8c779e8c2c34a55a6b940d7d5c05ae012fa1c
imranmalakzai <afghanimran92@gmail.com>
2026-02-14 18:44:48 +0430

    feat(post): add react to post controller

---
+++ src/controllers/likes.controller.js
@@ -0,0 +1,29 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+import * as userDb from "../repository/users.repository.js";
+import * as likeDb from "../repository/likes.repository.js";
+import * as postDb from "../repository/posts.repository.js";
+
+// Like a post
+export const like = asyncHandler(async (req, res) => {
+  const { postId } = req.params;
+
+  // post exist ?
+  const post = await postDb.findById(postId);
+  if (!post) throw new ApiError("Post not exist", 404);
+
+  //is already reacted ? if reacted Remove reaction else add Reaction
+  const reacted = await likeDb.findById(req.user.id, postId);
+
+  if (reacted) {
+    const result = await likeDb.remove(req.user.id, postId);
+    if (result === 0) throw new ApiError("Internal server error", 500);
+    return res.status(204);
+  }
+
+  // Add reaction
+  const result = await likeDb.create(req.user.id, postId);
+  if (result === 0) throw new ApiError("Internal server error", 500);
+
+  res.status(200).json({ message: "post liked successfully" });
+});
