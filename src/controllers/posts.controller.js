09bb18a4348f3dc15c2d3e118b70cb84c293dbd7
imranmalakzai <afghanimran92@gmail.com>
2026-02-15 20:15:35 +0430

    feat(post): create add a new post controller

---
+++ src/controllers/posts.controller.js
@@ -0,0 +1,41 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+
+import * as postDb from "../repository/posts.repository.js";
+import * as hashtagDb from "../repository/hashtags.repository.js";
+import * as postHashtagDb from "../repository/postHashtags.repository.js";
+
+// Create a new post
+export const createPost = asyncHandler(async (req, res) => {
+  const { caption, visibility } = req.body;
+  const image = req.file?.path;
+
+  //  Create the post
+  const post = await postDb.create({
+    caption,
+    image,
+    user_id: req.user.id,
+    visibility,
+  });
+
+  // Extract hashtags from caption (e.g., "#fun #coding")
+  const hashtags = caption.match(/#[a-zA-Z0-9_]+/g) || []; // returns array or empty
+
+  for (let tag of hashtags) {
+    let hashtagRecord = await hashtagDb.findByName(tag);
+
+    //  If hashtag doesn't exist, create it
+    if (!hashtagRecord) {
+      const newHashtag = await hashtagDb.create({ name: tag });
+      hashtagRecord = { id: newHashtag, name: tag };
+    }
+
+    //  Link post and hashtag
+    await postHashtagDb.create({
+      post_id: post,
+      hashtag_id: hashtagRecord.id,
+    });
+  }
+
+  res.status(201).json({ message: "Post created successfully" });
+});
