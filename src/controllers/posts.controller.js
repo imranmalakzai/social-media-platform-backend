3dda461035134fd353362632649f7dd92ab3a356
imranmalakzai <afghanimran92@gmail.com>
2026-02-15 21:11:41 +0430

    feat(post): create update post controller

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -39,3 +39,41 @@ export const createPost = asyncHandler(async (req, res) => {
 
   res.status(201).json({ message: "Post created successfully" });
 });
+
+// Update post
+export const updatePost = asyncHandler(async (req, res) => {
+  const { postId } = req.params;
+  const image = req.file.path;
+  const { caption } = req.body;
+
+  //post exist
+  const post = await postDb.findById(postId);
+  if (!post || post.user_id.toString() !== req.user.id.toString()) {
+    throw new ApiError("Invalid Request", 400);
+  }
+
+  // update
+  const update = postDb.updatePost(caption, image, postId);
+
+  // Extract hashtags from caption
+  const hashtags = caption.match(/#[a-zA-Z0-9_]+/g) || [];
+
+  for (let tag of hashtags) {
+    let hashtagRecord = await hashtagDb.findByName(tag);
+
+    //  If hashtag doesn't exist, create it
+    if (!hashtagRecord) {
+      const newHashtag = await hashtagDb.create({ name: tag });
+      hashtagRecord = { id: newHashtag, name: tag };
+    }
+
+    //  Link post and hashtag
+    await postHashtagDb.create({
+      post_id: post,
+      hashtag_id: hashtagRecord.id,
+    });
+  }
+
+  if (update === 0) throw new ApiError("Internal server error", 500);
+  res.status(200).json({ message: "post updated succesflly" });
+});
