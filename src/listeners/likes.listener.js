cf33bd4c1392fbcf65725d959b49ca34a432782b
imranmalakzai <afghanimran92@gmail.com>
2026-02-16 21:24:14 +0430

    fix(likes): Replace console error with winston logger error

--- src/listeners/likes.listener.js
+++ src/listeners/likes.listener.js
@@ -1,5 +1,6 @@
 import * as notificationsDb from "../repository/notifications.repository.js";
 import { eventBus } from "../events/eventBus.js";
+import { logger } from "../config/logger.config.js";
 
 eventBus.on("post.liked", async ({ postId, sender_id, resiver_id }) => {
   try {
@@ -9,6 +10,12 @@ eventBus.on("post.liked", async ({ postId, sender_id, resiver_id }) => {
       resiver_id,
     });
   } catch (error) {
-    console.log("Error inside the like listener");
+    logger.error("Notification creation failed in like a post", {
+      postId,
+      sender_id,
+      resiver_id,
+      error: error.message,
+      stack: error.stack,
+    });
   }
 });
