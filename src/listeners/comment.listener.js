d990050148f2cc37780cc01c25c1bdfb74f40d7a
imranmalakzai <afghanimran92@gmail.com>
2026-02-16 21:18:08 +0430

    fix(comments): Replace console error with logger for notification creation failures

--- src/listeners/comment.listener.js
+++ src/listeners/comment.listener.js
@@ -1,15 +1,22 @@
 import * as notificationsDb from "../repository/notifications.repository.js";
+import { logger } from "../config/logger.config.js";
 import { eventBus } from "../events/eventBus.js";
 
 eventBus.on("COMMET", async (data) => {
   try {
     await notificationsDb.create({
       sender_id: data.sender_id,
-      resiver_id: data.resiver_id,
+      recipient_id: data.recipient_id,
       type: data.type,
       post_id: data.post_id,
     });
   } catch (error) {
-    console.log("Error in notification comment listener");
+    // professional logging instead of console.error
+    logger.error("Notification creation failed", {
+      senderId: data.sender_id,
+      recipientId: data.recipient_id,
+      error: error.message,
+      stack: error.stack,
+    });
   }
 });
