de572aaa0472319c395c4d36d7b44e0a811fe31d
imranmalakzai <afghanimran92@gmail.com>
2026-02-17 12:49:41 +0430

    feat(users): Include email in USER.REGISTERED event on user registration

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -19,7 +19,7 @@ export const register = asyncHandler(async (req, res) => {
   if (result === 0) throw new ApiError("Internal server error", 500);
 
   setImmediate(() => {
-    eventBus.evmit("USER.REGISTERED", { user_id: result });
+    eventBus.evmit("USER.REGISTERED", { user_id: result, email });
   });
 
   res.status(201).json({ message: "Account create . opt sent to your email" });
