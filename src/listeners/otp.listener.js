6c017dd6ac47f097db786891b50f41fcb992865f
imranmalakzai <afghanimran92@gmail.com>
2026-02-17 12:49:12 +0430

    feat(otp): Implement OTP generation and email sending on user registration

---
+++ src/listeners/otp.listener.js
@@ -0,0 +1,23 @@
+import { eventBus } from "../eventBus.js";
+import { logger } from "../config/logger.config.js";
+import * as otpDb from "../repository/opt.repository.js";
+import { sendOtpEmail } from "../services/email.services.js";
+import * as userDb from "../repository/users.repository.js";
+import crypto from "crypto";
+
+// Send otp message for verification
+eventBus.on("USER.REGISTERED", async ({ userId, email }) => {
+  try {
+    const otp = Math.floor(100000 + Math.random() * 900000).toString(); // 6-digit OTP
+    const otpHash = crypto.createHash("sha256").update(otp).digest("hex");
+    await otpDb.create(userId, otpHash, "VERIFY_EMAIL");
+    // send otp email
+    await sendOtpEmail(email, otp);
+  } catch (error) {
+    logger.error("Error in send opt event", {
+      userId,
+      error: error.message,
+      stack: error.stack,
+    });
+  }
+});
