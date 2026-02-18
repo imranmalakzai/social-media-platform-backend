cfd3197d438a8d7958f12d44997631b18e05a57e
imranmalakzai <afghanimran92@gmail.com>
2026-02-18 10:04:24 +0430

    feat(auth): Add reset password in EDA

--- src/listeners/otp.listener.js
+++ src/listeners/otp.listener.js
@@ -2,7 +2,6 @@ import { eventBus } from "../eventBus.js";
 import { logger } from "../config/logger.config.js";
 import * as otpDb from "../repository/opt.repository.js";
 import { sendOtpEmail } from "../services/email.services.js";
-import * as userDb from "../repository/users.repository.js";
 import crypto from "crypto";
 
 // Send otp message for verification
@@ -16,6 +15,24 @@ eventBus.on("USER.REGISTERED", async ({ userId, email }) => {
   } catch (error) {
     logger.error("Error in send opt event", {
       userId,
+      error: error.message,
+      stack: error.stack,
+    });
+  }
+});
+
+eventBus.on("PASSWORD.RESET", async ({ userId, email }) => {
+  try {
+    const otp = Math.floor(100000 * Math.random() * 900000).toString();
+    const otpHash = crypto.createHash("sha256").update(otp).digest("hex");
+    // save in db
+    await otpDb.create(userId, otpHash, "RESET_PASSWORD");
+    // send email
+    await sendOtpEmail(email, otp);
+  } catch (error) {
+    logger.error("ERROR IN RESET PASSWORD listener", {
+      userId,
+      email,
       error: error.message,
       stack: error.stack,
     });
