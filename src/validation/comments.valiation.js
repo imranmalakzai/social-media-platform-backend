f2f04e53d264bcfa097b47c1a63f0ddcdcc829b1
imranmalakzai <afghanimran92@gmail.com>
2026-02-20 11:37:36 +0430

    fix(validation): correct identiation in create post zod validation schema

--- src/validation/comments.valiation.js
+++ src/validation/comments.valiation.js
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const createPost = z.object({
-  parent_comment_id : z.string("parent comment id is required");
-  text: z.string().min(1,"comment text is required")
-})
+  parent_comment_id: z.string("parent comment id is required"),
+  text: z.string().min(1, "comment text is required"),
+});
