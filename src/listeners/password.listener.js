13ea8a6f565d15d60db0bbe46f10fa8d76a18bd9
imranmalakzai <afghanimran92@gmail.com>
2026-02-20 08:48:08 +0430

    feat(auth): Add password reset event driven lisener

---
+++ src/listeners/password.listener.js
@@ -0,0 +1,19 @@
+import { eventBus } from "../events/eventBus.js";
+import { sendOtpEmail } from "../services/email.services.js";
+import { logger } from "../config/logger.config.js";
+import * as otpDb from "../repository/otp.repository.js";
+import crypto from "crypto";
+
+eventBus.on("RESET.PASSWORD", async ({ userId, email }) => {
+  try {
+    const otp = Math.floor(100000 * Math.random() * 6000000);
+    const hashOtp = crypto.createHash("sha256").update(otp).digest("hex");
+    await otpDb.create(userId, hashOtp, "RESET_PASSWORD");
+    sendOtpEmail(email, otp);
+  } catch (error) {
+    logger.error("Error in Reset password listener", {
+      error: error.message,
+      stack: error.stack,
+    });
+  }
+});
