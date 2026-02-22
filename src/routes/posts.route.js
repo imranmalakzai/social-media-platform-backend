d38902fdcb09d1b4d454e44610488258657802f3
imranmalakzai <afghanimran92@gmail.com>
2026-02-22 09:19:07 +0430

    feat(posts): Add public post api route endpoints

---
+++ src/routes/posts.route.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { validate } from "../middleware/zod.middleware.js";
+import { auth } from "../middleware/auth.middleware.js";
+import * as Posts from "../controllers/posts.controller.js";
+import * as schema from "../validation/posts.validation.js";
+
+// initialize post router
+const postRouter = express.Router();
+
+//Get all public posts
+postRouter.route("/posts").get(Posts.getPublicPosts);
+
+// Get a post by Id
+postRouter.route("/posts/:postId").get(Posts.getAPostById);
