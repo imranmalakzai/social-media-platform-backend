8e85658a94b16d65852faec8388f98b0ab74c437
imranmalakzai <afghanimran92@gmail.com>
2026-02-22 19:00:04 +0430

    feat(auth): validate comment route api end point

--- src/routes/comments.route.js
+++ src/routes/comments.route.js
@@ -3,13 +3,16 @@ import express from "express";
 import { auth } from "../middleware/auth.middleware.js";
 import * as comments from "../controllers/comments.controller.js";
 import * as schema from "../validation/comments.valiation.js";
+import { validate } from "../middleware/zod.middleware.js";
 
 const commentRouter = express.Router({ mergeParams: true });
 
 commentRouter.use(auth);
 
 // create comments
-commentRouter.route("/comments").post(comments.createComment);
+commentRouter
+  .route("/comments")
+  .post(validate(schema.createPost), comments.createComment);
 
 // Get all comments
 commentRouter.route("/comments").get(comments.postComments);
