460acee640068af3b9e7df7c5a364438a5d67a76
imranmalakzai <afghanimran92@gmail.com>
2026-02-23 10:35:28 +0430

    chore(listeners): add opt and password listners path into centralize module

--- src/listeners/index.js
+++ src/listeners/index.js
@@ -2,3 +2,5 @@ import "./comment.listener.js";
 import "./followers.listener.js";
 import "./likes.listener.js";
 import "./post.listener.js";
+import "./password.listener.js";
+import "./otp.listener.js";
