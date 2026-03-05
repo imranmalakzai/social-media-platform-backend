8a86da86121dfc599b043264506f04eba60a6765
imranmalakzai <afghanimran92@gmail.com>
2026-03-05 09:46:37 +0430

    feat(notification): Add get a notification by db query

--- src/controllers/notifications.controller.js
+++ src/controllers/notifications.controller.js
@@ -1,6 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { addClient, removeClient } from "../SSE/sseManager.js";
 import * as notificationDb from "../repository/notifications.repository.js";
+import ApiError from "../utils/ApiError.js";
 
 // Server side notification stream
 export const notificationStream = asyncHandler(async (req, res) => {
@@ -26,4 +27,15 @@ export const notificationStream = asyncHandler(async (req, res) => {
 export const notifications = asyncHandler(async (req, res) => {
   const notifications = await notificationDb.findAll(req.user.id);
   res.status(200).json({ notifications: notifications || [] });
+});
+
+// Get a notifications by Id
+export const notification = asyncHandler(async (req, res) => {
+  const { notificationId } = req.params;
+
+  // notification exist
+  const notification = await notificationDb.findById(notificationId);
+  if (!notification) throw new ApiError("Notification not exist", 404);
+
+  res.status(200).json({ notification });
 });
